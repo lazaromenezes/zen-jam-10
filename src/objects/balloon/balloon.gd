2133ba3ab71038f0573e96c928ab68a6b6867961
tumati <jgsoaresdias@gmail.com>
2025-02-04 19:48:14 -0300

    Shake balloon when inflating

--- src/objects/balloon/balloon.gd
+++ src/objects/balloon/balloon.gd
@@ -9,16 +9,20 @@ signal released
 @export var _sprite: Sprite2D
 @export var _min_scale: float = 0.5
 @export var _max_scale: float = 1
-@export var _h_speed: float = 350
+@export var _h_speed: float = 320
 @export var _gravity: float = 200
 @export var _points_scene: PackedScene
 @export var _ballons: Array[Texture2D]
+@export var _sprite_pivot: Node2D
+@export var _max_shake_rotation_deg: float = 45
+@export var _shake_speed: float = 10
 
 var _inflating: bool = false
 var _size: float = 0
 var _released: bool = false
 var _v_speed: float
 var _stall: Stall
+var _shake_dir: int = 1
 
 func _ready() -> void:
 	_sprite.texture = _ballons.pick_random()
@@ -27,7 +31,7 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if not _released and Input.is_action_just_pressed("ui_accept"):
 		_inflating = true
-	if not _released and Input.is_action_just_released("ui_accept"):
+	if not _released and _inflating and Input.is_action_just_released("ui_accept"):
 		_stop_inflating()
 	
 	if _inflating:
@@ -51,7 +55,15 @@ func _inflate(delta: float) -> void:
 		_pop()
 	else:
 		_update_scale()
-		_update_color()
+		_shake(delta)
+
+func _shake(delta: float) -> void:
+	_sprite_pivot.rotate(_shake_dir * _shake_speed * delta)
+	var amplitude := inverse_lerp(_max_size, 0, _size) * _max_shake_rotation_deg
+	if _sprite_pivot.rotation >= deg_to_rad(amplitude):
+		_shake_dir = -1
+	if _sprite_pivot.rotation <= deg_to_rad(-amplitude):
+		_shake_dir = 1
 
 func _update_scale() -> void:
 	var sprite_scale := lerpf(_min_scale, _max_scale, _size/_max_size)
@@ -69,6 +81,7 @@ func _release_ballon() -> void:
 	_released = true
 	_v_speed = -_size * 5
 	_sprite.self_modulate = Color.WHITE
+	_sprite_pivot.rotation = 0
 	released.emit()
 
 func _pop() -> void:
@@ -83,6 +96,6 @@ func _spawn_points(value: int) -> void:
 	points.global_position = global_position
 	points.get_node("Label").text = str(value)
 
-func _on_area_2d_area_entered(area: Area2D) -> void:
+func _on_area_2d_area_entered(_area: Area2D) -> void:
 	_spawn_points(int(global_position.distance_to(_stall.global_position)))
-	_pop()
+	queue_free()
--- src/objects/stall/stall.gd
+++ src/objects/stall/stall.gd
@@ -13,6 +13,12 @@ func _create_balloon() -> void:
 	balloon.set_stall(self)
 	balloon.global_position = global_position
 	balloon.released.connect(_on_balloon_released)
+	balloon.pop.connect(_on_balloon_popped)
 
 func _on_balloon_released() -> void:
+	await get_tree().create_timer(1).timeout
+	_create_balloon.call_deferred()
+
+func _on_balloon_popped() -> void:
+	await get_tree().create_timer(1).timeout
 	_create_balloon.call_deferred()
