5321f6326d20972632401d6f19638ab08af9bc3a
Lazaro Menezes <lazarodm@gmail.com>
2025-02-08 19:02:01 -0300

    Implement transition to main game screen

---
+++ src/objects/scene_manager/scene_manager.gd
@@ -0,0 +1,36 @@
+extends CanvasLayer
+
+enum Scene{
+	TITLE,
+	MAP
+}
+
+@export var _scenes: Dictionary[Scene, PackedScene] = {}
+@export var _ballons: Array[Texture2D]
+
+var _spawn_position: Vector2
+
+func _ready() -> void:
+	var width = ProjectSettings.get_setting("display/window/size/viewport_width")
+	var height = ProjectSettings.get_setting("display/window/size/viewport_height")
+	
+	_spawn_position = Vector2(width, height) / 2
+
+func transition_to(new_scene: Scene) -> void:
+	var sprite := _spawn_sprite()
+	
+	var tween = create_tween()
+	tween.tween_property(sprite, "scale", Vector2(20, 20), 0.5)
+	tween.tween_callback(func(): _switch_scene(new_scene)).set_delay(0.1)
+	tween.tween_property(sprite, "scale", Vector2.ONE, 0.5)
+	tween.tween_callback(func(): sprite.queue_free())
+
+func _spawn_sprite() -> Sprite2D:
+	var sprite = Sprite2D.new()
+	add_child(sprite)
+	sprite.texture = _ballons.pick_random()
+	sprite.position = _spawn_position
+	return sprite
+
+func _switch_scene(scene: Scene) -> void:
+	get_tree().change_scene_to_packed(_scenes[scene])
--- src/screens/title/title_screen.gd
+++ src/screens/title/title_screen.gd
@@ -4,8 +4,9 @@ func _ready() -> void:
 	for option: Button in %MenuContainer.get_children():
 		option.focus_entered.connect(_menu_option_focus_entered.bind(option))
 	
-	_select_first_option.call_deferred()
+	%Exit.visible = not OS.has_feature("web")
 	
+	_select_first_option.call_deferred()
 
 func _menu_option_focus_entered(target: Button) -> void:
 	%SelectionIndicator.set_placement(target)
@@ -13,3 +14,12 @@ func _menu_option_focus_entered(target: Button) -> void:
 func _select_first_option() -> void:
 	%SinglePlayer.grab_focus()
 	%SelectionIndicator.show()
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
+
+func _on_single_player_pressed() -> void:
+	SceneManager.transition_to(SceneManager.Scene.MAP)
+
+func _on_options_pressed() -> void:
+	pass # Replace with function body.
