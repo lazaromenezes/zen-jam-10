01de20d30e125655f627cac0e8441fb92aeeded1
tumati <jgsoaresdias@gmail.com>
2025-02-10 18:59:08 -0300

    Game over on oversized line

--- src/objects/line_manager/line_manager.gd
+++ src/objects/line_manager/line_manager.gd
@@ -1,11 +1,15 @@
 class_name LineManager
 extends Node2D
 
+signal updated_line(count: int)
+
 @export var _kid_scene: PackedScene
 @export var _initial_delay: float = 8
 @export var _timer: Timer
+@export var _max_kids_inline: int = 7
 
 var _current_delay: float
+var _line_count: int
 
 func _ready() -> void:
 	_spawn_kid.call_deferred()
@@ -17,6 +21,21 @@ func _spawn_kid() -> void:
 	var kid := _kid_scene.instantiate() as Kid
 	add_sibling(kid)
 	kid.global_position = global_position
+	kid.enter_line.connect(_on_enter_line)
+	kid.bouncing.connect(_on_bouncing)
+
+func _game_over() -> void:
+	print("Game Over!!")
 
 func _on_timeout() -> void:
 	_spawn_kid()
+
+func _on_enter_line() -> void:
+	_line_count += 1
+	if _line_count >= _max_kids_inline:
+		_game_over()
+	updated_line.emit(_line_count)
+
+func _on_bouncing() -> void:
+	_line_count -= 1
+	updated_line.emit(_line_count)
