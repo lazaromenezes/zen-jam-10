63c3340829d34c668515644e609977f43d2673b8
Lazaro Menezes <lazarodm@gmail.com>
2025-02-13 23:52:36 -0300

    Implement game over button actions

--- src/objects/floating_board/floating_board.gd
+++ src/objects/floating_board/floating_board.gd
@@ -6,11 +6,9 @@ class_name FloatingBanner
 func _ready() -> void:
 	%LeftBaloon.texture = _icons.pick_random()
 	%RightBaloon.texture = _icons.pick_random()
+	%Exit.visible = not OS.has_feature("web")
 	for button in %Buttons.get_children():
 		button.focus_entered.connect(_on_button_focus.bind(button))
-		
-func _get_configuration_warnings() -> PackedStringArray:
-	return ["This node requires any Control as children"]
 
 func select_first_option() -> void:
 	%PlayAgain.grab_focus()
@@ -18,3 +16,12 @@ func select_first_option() -> void:
 
 func _on_button_focus(button: Button) -> void:
 	%SelectionIndicator.set_placement(button)
+
+func _on_play_again_pressed() -> void:
+	SceneManager.transition_to(SceneManager.Scene.MAP)
+
+func _on_title_pressed() -> void:
+	SceneManager.transition_to(SceneManager.Scene.TITLE)
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
